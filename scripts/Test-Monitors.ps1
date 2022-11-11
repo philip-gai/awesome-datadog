d33fbbc1a6019fe9a149db5a4f60d3838d64df76
Philip Gai <philip-gai@github.com>
2022-11-11 13:17:43 -0600

    Add Test-Monitors.ps1

---
+++ scripts/Test-Monitors.ps1
@@ -0,0 +1,36 @@
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory = $false)]
+  [string]$ApiKey = $env:DD_API_KEY,
+  [Parameter(Mandatory = $false)]
+  [string]$AppKey = $env:DD_APP_KEY
+)
+
+$ErrorActionPreference = "Stop"
+
+Import-Module -Name $PSScriptRoot/AwesomeDatadog/AwesomeDatadog.psm1 -Force
+
+# Get all *.json files in the monitors directories
+Write-Host "Getting all monitors from the monitors directories"
+$monitorFiles = Get-ChildItem -Path $PSScriptRoot/../*/monitors/*.json -Recurse
+
+Write-Host "Found $($monitorFiles.Count) monitors"
+Write-Debug "Monitor files: $monitorFiles"
+
+$invalidMonitors = @()
+
+foreach ($monitorFile in $monitorFiles) {
+  # Validate the monitor
+  $isValid = Test-Monitor -MonitorFile $monitorFile.FullName -ApiKey $ApiKey -AppKey $AppKey -Debug:$DebugPreference
+  if (!$isValid) {
+    Write-Warning "Monitor `"$($monitorFile.FullName)`" is invalid"
+    $invalidMonitors += $monitorFile.FullName
+  }
+}
+
+Write-Host "Done validating the monitors 🧪"
+
+if ($invalidMonitors.Count -gt 0) {
+  Write-Warning "Invalid monitors:`n$($invalidMonitors | Out-String)"
+  exit 1
+}
